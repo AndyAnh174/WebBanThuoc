66e49c36affa63e0a1e9734099ffdfd6cd00a161
Hồ Việt Anh <hovietanh147@gmail.com>
2025-12-28 10:23:27 +0700

    change port

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -2,6 +2,6 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   nodeEnv: process.env.NODE_ENV || 'development',
-  port: parseInt(process.env.PORT || '3000', 10),
+  port: parseInt(process.env.PORT || '3001', 10),
   corsOrigins: process.env.CORS_ORIGINS?.split(',') || ['http://localhost:3001'],
 }));
